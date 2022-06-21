a88533e73818503ffea48866a343792cd7df5401
hanna244 <shhn0509@gmail.com>
2022-06-21 23:56:03 +0900

    update: store 세팅

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,9 +1,37 @@
-const { createWrapper } = require("next-redux-wrapper");
+import { createWrapper } from 'next-redux-wrapper';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
-const configureStore = () => {};
+import reducer from '../reducers';
+import rootSaga from '../sagas';
 
-const wrapper = createWrapper(configureStore), {
-  debug: process.env.NODE_ENV = 'develropment'
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  // redux-saga, redux-thunk 등을 넣는다.
+  const middlewares = [sagaMiddleware, loggerMiddleware];
+  //개발 모드임을 확인
+  const enhancer =
+    process.env.NODE_ENV === 'production'
+      ? // 개발 모드일 경우 redux-devtools 실행
+        compose(applyMiddleware(...middlewares))
+      : // 개발 모드일 경우 redux-devtools 실행 안됨
+        composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
 };
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === 'development',
+});
 
 export default wrapper;
